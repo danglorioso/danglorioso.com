28c7234418f930f220e74ad2b79e05a151c1475f
Dan Glorioso <125602370+danglorioso@users.noreply.github.com>
2024-09-26 01:36:37 -0400

    Fixing Vercel analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,9 @@
 import { defineConfig } from 'astro/config';
+import vercel from '@astrojs/vercel/serverless';
 import tailwind from "@astrojs/tailwind";
 
 import react from "@astrojs/react";
 
-import vercel from '@astrojs/vercel/serverless';
 
 // https://astro.build/config
 export default defineConfig({
