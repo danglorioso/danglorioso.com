c500f57b2db8b20ed08b1301849e8929398f65b3
Dan Glorioso <125602370+danglorioso@users.noreply.github.com>
2024-09-26 01:22:09 -0400

    Added Web Analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,13 @@ import tailwind from "@astrojs/tailwind";
 
 import react from "@astrojs/react";
 
+import vercel from '@astrojs/vercel/serverless';
+
 // https://astro.build/config
 export default defineConfig({
+  output: 'server',
+  adapter: vercel({
+    webAnalytics: { enabled: true }
+  }),
   integrations: [tailwind(), react()]
 });
