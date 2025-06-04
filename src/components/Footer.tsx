93a2a75119b82b6a789d7d5cf5ec20225a1c35ed
Dan Glorioso <125602370+danglorioso@users.noreply.github.com>
2025-06-04 01:43:26 -0400

    Adding footer

---
+++ src/components/Footer.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+const Footer: React.FC = () => {
+  return (
+    <footer className="fixed bottom-0 left-0 w-full text-center py-3 border-t border-gray-800 z-50">
+      <p className="text-md text-gray-400">
+        Copyright © {new Date().getFullYear()}. Created by{" "}
+        <a
+          href="https://danglorioso.com"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="text-blue-500 hover:text-blue-700"
+        >
+          Dan Glorioso
+        </a>
+        .
+      </p>
+      {/* <p className="text-xs text-gray-500 mt-1">Copyright © 2025. All rights reserved.</p> */}
+    </footer>
+  );
+};
+
+export default Footer;
