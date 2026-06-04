b1ead0e6e42876f77ce48e346b3507a346a625e7
Dan Glorioso <125602370+danglorioso@users.noreply.github.com>
2026-06-04 16:39:58 -0400

    chore: update dependencies & add PostCSS config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,5 @@
 import { defineConfig } from 'astro/config';
 import vercel from '@astrojs/vercel';
-import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
 import mdx from "@astrojs/mdx";
 
@@ -10,5 +9,5 @@ export default defineConfig({
   adapter: vercel({
     webAnalytics: { enabled: true }
   }),
-  integrations: [tailwind(), react(), mdx()]
+  integrations: [react(), mdx()]
 });
